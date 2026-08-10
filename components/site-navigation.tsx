a59dbcab06c9f5fd5848be0c8be550095f8c3cf4
James Serritslev <jamesserritslev@gmail.com>
2026-08-10 16:11:46 -0600

    Done

--- components/site-navigation.tsx
+++ components/site-navigation.tsx
@@ -1,8 +1,18 @@
 import { getSiteImagery } from "@/lib/sanity/site-imagery"
+import { getLayoutSingletons } from "@/lib/sanity/layout-singletons"
 import { Navigation } from "@/components/navigation"
+import { DEFAULT_HERO_META_HOURS } from "@/lib/content-defaults"
 
 export async function SiteNavigation() {
-  const { siteLogoUrl } = await getSiteImagery()
+  const [{ siteLogoUrl }, L] = await Promise.all([
+    getSiteImagery(),
+    getLayoutSingletons(),
+  ])
 
-  return <Navigation logoSrc={siteLogoUrl} />
+  return (
+    <Navigation
+      logoSrc={siteLogoUrl}
+      hoursLine={L.home?.heroMetaHours?.trim() || DEFAULT_HERO_META_HOURS}
+    />
+  )
 }
